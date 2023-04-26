fda7b866aa751b7afd930c9b0c857eab5f985de9
Greg Hogan <me@greghogan.uk>
2023-04-26 14:26:07 +0200

    Fix unstyled flash
    - Add loading of styled components into the custom
      _document page to prevent flash of unstyled content.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,47 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700&display=swap"
-          rel="stylesheet"
-        />
+import { ServerStyleSheet } from 'styled-components';
 
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const CustomDocument = () => (
+  <Html lang="en">
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700&display=swap"
+        rel="stylesheet"
+      />
 
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+CustomDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: [initialProps.styles, sheet.getStyleElement()],
+    };
+  } finally {
+    sheet.seal();
+  }
+};
+
+export default CustomDocument;
